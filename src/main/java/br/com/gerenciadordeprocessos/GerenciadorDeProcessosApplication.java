5cc3fc1de4bba34bc72384b0dd55b6f18dbd1b1c
Emanuel Cavalcante <emanuellima3105@gmail.com>
2020-01-30 17:10:09 -0300

    Configurando conexão com o banco de dados para o postgresql Inserindo a query prara criar o banco de dados

--- src/main/java/br/com/gerenciadordeprocessos/GerenciadorDeProcessosApplication.java
+++ src/main/java/br/com/gerenciadordeprocessos/GerenciadorDeProcessosApplication.java
@@ -1,7 +1,5 @@
 package br.com.gerenciadordeprocessos;
 
-import br.com.gerenciadordeprocessos.security.enums.PerfilEnum;
-import br.com.gerenciadordeprocessos.usuario.modelos.Usuario;
 import br.com.gerenciadordeprocessos.usuario.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -11,8 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableJpaRepositories({"br.com.gerenciadordeprocessos.usuario.repository", "br.com.gerenciadordeprocessos.processo.repository",
@@ -29,38 +25,6 @@ public class GerenciadorDeProcessosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GerenciadorDeProcessosApplication.class, args);
-    }
-
-    // Inserindo usuarios padroes
-    @PostConstruct
-    void started() {
-        Usuario usuario1 = new Usuario();
-        usuario1.setAtivo(Boolean.TRUE);
-        usuario1.setNome("Usuario administrador");
-        usuario1.setPerfil(PerfilEnum.ADMINISTRADOR);
-        usuario1.setLogin("loginadministrador");
-        // 123456
-        usuario1.setSenha("$2a$10$xo0Avm7fk/1IODbPzvFea.wUg5JhNmBd8ptRJzKjuzeoXIX0N6UGS");
-        repository.save(usuario1);
-
-        Usuario usuario2 = new Usuario();
-        usuario2.setAtivo(Boolean.TRUE);
-        usuario2.setNome("Usuario finalizador");
-        usuario1.setPerfil(PerfilEnum.USUARIO_FINALIZADOR);
-        usuario2.setLogin("loginfinalizador");
-        // 123456
-        usuario2.setSenha("$2a$10$xo0Avm7fk/1IODbPzvFea.wUg5JhNmBd8ptRJzKjuzeoXIX0N6UGS");
-        repository.save(usuario2);
-
-        Usuario usuario3 = new Usuario();
-        usuario3.setAtivo(Boolean.TRUE);
-        usuario3.setNome("Usuario triador");
-        usuario1.setPerfil(PerfilEnum.USUARIO_TRIADOR);
-        usuario3.setLogin("logintriador");
-        // 123456
-        usuario3.setSenha("$2a$10$xo0Avm7fk/1IODbPzvFea.wUg5JhNmBd8ptRJzKjuzeoXIX0N6UGS");
-        repository.save(usuario3);
-
     }
 
     @GetMapping("/")
